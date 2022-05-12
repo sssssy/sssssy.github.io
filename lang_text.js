364b70bd43f7c58259ef1ebdb0987202dde52cea
JiahuiFan <fjh@njust.edu.cn>
2022-05-12 18:39:05 +0800

    add lang jp;

--- lang_text.js
+++ lang_text.js
@@ -10,6 +10,7 @@ var arrLang = {
                 Nanjing, China \n \
             ",
         "Also find me at:": "Also find me at:",
+        "E-mail:": "E-mail:",
         "hello, there": "< Hello, there!",
         "intro": "\
             I am Jiahui Fan, a Ph.D. candidate in PCA Lab of NJUST. I am supervised by Prof. Beibei Wang and Prof. Jian Yang. As a young Padawan in Graphics, I started my journey in Graphics since my first year of Ph.D. in 2020, while my former major was in the field of Computer Vision. I've always been interested in understanding and utilizing the principle how everything works in both the real world and the virtual ones. It will be my greatest honor (and also a desire) to communicate with you - who share the same interest and passion with me. Please feel free to contact me on any thoughts, questions, or comments. \
@@ -35,6 +36,7 @@ var arrLang = {
                 江苏省南京市\n \
             ",
         "Also find me at:": "您也可以在以下页面找到我:",
+        "E-mail:": "邮箱:",
         "hello, there": "< “你好啊！”",
         "intro": "\
             我是樊家辉，南京理工大学PCA Lab的博士研究生。我的导师是杨健教授和王贝贝教授，目前主要的研究方向为真实感渲染与人工智能。我本科的专业方向是计算机视觉，从博士一年级（2020年）开始接触渲染领域，也完成了一些渲染和神经网络相结合的工作。我对于现实世界乃至虚拟世界的一切背后原理都深感兴趣，并期待着能和拥有同样的爱好和热情的朋友交流。如果有任何想法、问题或者建议，都可以与我联系！ \
@@ -50,5 +52,31 @@ var arrLang = {
         "supplementary": "补充材料",
         "video": "视频",
         "URL": "链接",
+    },
+    "jp": {
+        "name": "Jiahui Fan (樊　家輝)",
+        "top-intro": "\
+                博士課程大学院生\n\n \
+                高次元情報知能感知とシステム、教育部重点実験室 PCA Lab\n \
+                南京理工大学\n \
+                中国南京\n \
+            ",
+        "Also find me at:": "以下のページで私を見つけることもできます:",
+        "E-mail:": "メール:",
+        "hello, there": "< 「こんにちは！」",
+        "intro": "\
+                私は樊家輝と申します。南京理工大学のPCA Labの博士課程大学院生です。私のメンターは楊健教授と王貝貝教授であります。今、私は主にコンピュータグラフィックス（CG）と機械学習に関する研究を行っています。この前に、コンピュータビジョンを専攻していましたが、博士課程の1年生(2020年)頃からCGの分野に取り組み始め、関連する論文もいくつか完了しました。現実の世界と仮想の世界の両方ですべてがどのように機能するかという原則を理解し、活用することに、私は常に深い興味を持っています。そして、私と同じ趣味と情熱を持つ友達と交流することをぜひとも願っています。ご質問やお問い合わせなどもご自由にどうぞ！ \
+        ",
+        "What am I doing?": "研究方向",
+        "my research": "私の研究方向レンダリングとコンピュータビジョンです。主に：",
+        "Neural-based Appearance Modeling.": "「ニューラルネットワークの材質表現モデリング」",
+        "Micro-facet Model.": "「マイクロファセット　モデル」",
+        "Sparse Depth Completion.": "「スパースデプスコンプリーション」",
+        "Featured Publications": "発表文献",
+        "*: corresponding author(s).": "（*: 責任著者)",
+        "paper": "論文",
+        "supplementary": "サプリメンタル",
+        "video": "ビデオ",
+        "URL": "リンク",
     }
 };
